b8e2041b07171a2eeb8920cdcc4c007a3f965d69
Xing gao <738234123@qq.com>
2025-02-07 15:33:28 -0500

    post

--- routers/purchasing.router.ts
+++ routers/purchasing.router.ts
@@ -4,6 +4,6 @@ import { purchaseMaterial } from "../controllers/purchasing.controller";
 
 const purchasingRouter = express.Router();
 
-purchasingRouter.put("/purchasing-material", isAutheticated, authorizeRoles("管理"), purchaseMaterial);
+purchasingRouter.post("/purchasing-material", isAutheticated, authorizeRoles("管理"), purchaseMaterial);
 
 export default purchasingRouter;
